21f35714006cc9c4a86877bbf4f95ef76c0951cc
mrrohanp <rohanpote0@gmail.com>
2022-02-08 21:25:05 +0530

    #TA-003: add local storage

--- src/store/store.js
+++ src/store/store.js
@@ -2,5 +2,13 @@ import { createStore, applyMiddleware } from 'redux';
 import rootReducer from './reducers/index';
 
 import logger from 'redux-logger';
+import { readState, debouncedSaveState } from './localStorage';
 
-export default createStore(rootReducer, applyMiddleware(logger));
+const store = createStore(rootReducer, readState(), applyMiddleware(logger));
+
+store.subscribe(() => {
+  const state = store.getState();
+  debouncedSaveState(state);
+});
+
+export default store;
